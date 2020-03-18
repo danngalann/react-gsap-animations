c795b0b1053a8b1b19ec09d5362b93629f638c5a
ddasdasd <dasdasd@dasdas.com>
2020-03-18 18:04:00 +0100

    Implemented React Router. Added some styles.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Header from "./components/layout/header";
+import About from "./components/pages/About";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Route
+        exact
+        path="/"
+        render={() => {
+          return (
+            <div className="container">
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Incidunt, aliquid?
+              </p>
+            </div>
+          );
+        }}
+      />
+      <Route path="/about" component={About} />
+    </Router>
   );
 }
 
